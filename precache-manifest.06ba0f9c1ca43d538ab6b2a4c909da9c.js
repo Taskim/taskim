574ab73d09426da54e296908535bfaf8393d8d2a
Taskim <miksa_t@etna-alternance.net>
2019-03-16 21:59:01 +0100

    Updates

---
+++ precache-manifest.06ba0f9c1ca43d538ab6b2a4c909da9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed87acaa36687cc7c985",
+    "url": "/taskim/static/css/main.f8a4c742.chunk.css"
+  },
+  {
+    "revision": "ed87acaa36687cc7c985",
+    "url": "/taskim/static/js/main.ed87acaa.chunk.js"
+  },
+  {
+    "revision": "5fd39bab7f7996a45962",
+    "url": "/taskim/static/js/runtime~main.5fd39bab.js"
+  },
+  {
+    "revision": "eae7d38233d032134eab",
+    "url": "/taskim/static/js/2.eae7d382.chunk.js"
+  },
+  {
+    "revision": "eff8d38422a968540b517037810e8b43",
+    "url": "/taskim/static/media/crop-1.eff8d384.jpg"
+  },
+  {
+    "revision": "e675d3fecb9421bd540fac0a7fc33f28",
+    "url": "/taskim/index.html"
+  }
+];
