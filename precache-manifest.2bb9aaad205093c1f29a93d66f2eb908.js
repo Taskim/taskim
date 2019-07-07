1310b37fa8804ede3c8494695f36ddd138cdf3e8
Taskim <miksa_t@etna-alternance.net>
2019-07-07 22:15:29 +0200

    Updates

---
+++ precache-manifest.2bb9aaad205093c1f29a93d66f2eb908.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "953c33d02e19d3d06e8d4f9fa3eb98e2",
+    "url": "/taskim/index.html"
+  },
+  {
+    "revision": "252faa5e2e8b06bffa65",
+    "url": "/taskim/static/css/main.3f9a2a39.chunk.css"
+  },
+  {
+    "revision": "023562ead4b3540f1dd5",
+    "url": "/taskim/static/js/2.1f09659b.chunk.js"
+  },
+  {
+    "revision": "252faa5e2e8b06bffa65",
+    "url": "/taskim/static/js/main.520afb16.chunk.js"
+  },
+  {
+    "revision": "5fd39bab7f7996a45962",
+    "url": "/taskim/static/js/runtime~main.96fcc4f7.js"
+  },
+  {
+    "revision": "31d2c9b888fcc3784b27b347bf612e68",
+    "url": "/taskim/static/media/avatar.31d2c9b8.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/taskim/precache-manifest.af098930bb7377b3faf09cf960fe5c92.js"
+  "/taskim/precache-manifest.2bb9aaad205093c1f29a93d66f2eb908.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/taskim/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/taskim/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
